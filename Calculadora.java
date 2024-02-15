60a7942c066e9e196099ac8d3655dda463ab38a2
Andriy Symonenko Oliynyk <andriy.symonenko@iescelia.org>
2024-02-15 19:00:23 +0100

    Commit inicial

---
+++ Calculadora.java
@@ -0,0 +1,28 @@
+package ProyectoCalculadora;
+
+
+
+// Clase Calculadora
+class Calculadora {
+    private int numero1;
+    private int numero2;
+
+    public Calculadora(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public int realizarOperacion(String operacion) {
+        if (operacion.equals("suma")) {
+            return suma();
+        } else {
+            System.out.println("Operación no válida");
+            return 0;
+        }
+    }
+
+    public int suma() {
+        return numero1 + numero2;
+    }
+
+}
